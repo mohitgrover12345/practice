ef874f7d4f54101a5b897fcfd00683cd06aeba73
Mohit Grover <mohit.grover@mycom-osi.com>
2022-02-22 23:45:33 +0530

    Adding new Files

---
+++ src/test/java/org/test/devops/test.java
@@ -0,0 +1,19 @@
+package org.test.devops;
+
+import org.testng.annotations.Test;
+
+public class test {
+	
+	@Test
+	public void testc001()
+	{
+		System.out.println("Just checking the project ****");
+	}
+	
+	@Test
+	public void testc002()
+	{
+		System.out.println("****  Test ****");
+	}
+}
+
